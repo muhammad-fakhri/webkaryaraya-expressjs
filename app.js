dad59c066ad3ab689a2f8d0ad649f1bb8d5ac2c4
muhammad-fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-02-05 14:39:06 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+app.listen(PORT, () => {
+	console.log("Server is active. Ready to receive request from client");
+});
+
+//Array Data
+const name = ['fakhri', 'hilmi', 'bayu'];
+
+//Routes
+app.get('/', (req, res, next) => {
+	res.send(name);
+});
+
+//Get single data
+app.get('/:no', (req, res, next) => {
+	res.send(name[req.params.no]);
+});
+
+//Create new data
+app.post('/')
